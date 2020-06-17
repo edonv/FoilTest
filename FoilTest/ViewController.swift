d57a831c15db8172a76ca988ac3aa42d6aa016a4
edonv <22782929+edonv@users.noreply.github.com>
2020-06-17 18:34:01 -0400

    Initial Commit

---
+++ FoilTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoilTest
+//
+//  Created by Edon Valdman on 6/17/20.
+//  Copyright © 2020 Edon Valdman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
